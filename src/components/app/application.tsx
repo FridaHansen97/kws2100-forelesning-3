beb66ef28d1d1a553b15ae17478eee2390fd7157
frida <frida1997@live.no>
2026-01-19 14:30:16 +0100

    publish React to Github

--- src/components/app/application.tsx
+++ src/components/app/application.tsx
@@ -1,5 +1,23 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import { Map, View } from "ol";
+import TileLayer from "ol/layer/Tile.js";
+import { OSM } from "ol/source.js";
+
+const map = new Map({
+  layers: [new TileLayer({ source: new OSM() })],
+  view: new View({ zoom: 7, center: [7, 59] }),
+});
 
 export function Application() {
-  return <h1>Hello React</h1>;
+  const mapRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    map.setTarget(mapRef.current!);
+  }, []);
+
+  return;
+  <>
+    <h1>Kart over administrative områder i Norge</h1>
+    <div ref={mapRef}></div>
+  </>;
 }
